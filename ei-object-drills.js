4fae8c7903be381c366d2b2681fc1a39256b604a
Michael Verdi <michael.v.verdi@gmail.com>
2019-03-27 17:42:16 -0400

    finished Bonus hero questions

--- ei-object-drills.js
+++ ei-object-drills.js
@@ -168,13 +168,49 @@ function findOne(arr, query) {
   });
 }
 
-console.log(findOne(HEROES, { id: 1 }));
-// { id: 1, name: 'Captain America', squad: 'Avengers' }
-console.log(findOne(HEROES, { id: 10 }));
-// null
-console.log(findOne(HEROES, { id: 2, name: 'Aquaman' }));
-// null
-console.log(findOne(HEROES, { id: 5, squad: 'Justice League' }));
-// { id: 5, name: 'Wonder Woman', squad: 'Justice League' }
-console.log(findOne(HEROES, { squad: 'Justice League' }));
-// { id: 4, name: 'Superman', squad: 'Justice League' }
+// function findOne(arr, query) {
+//   return arr.find(hero => isSubset(hero, query));
+// }
+
+// function isSubset(obj1, obj2) {
+
+// }
+
+// console.log(findOne(HEROES, { id: 1 }));
+// // { id: 1, name: 'Captain America', squad: 'Avengers' }
+// console.log(findOne(HEROES, { id: 10 }));
+// // null
+// console.log(findOne(HEROES, { id: 2, name: 'Aquaman' }));
+// // null
+// console.log(findOne(HEROES, { id: 5, squad: 'Justice League' }));
+// // { id: 5, name: 'Wonder Woman', squad: 'Justice League' }
+// console.log(findOne(HEROES, { squad: 'Justice League' }));
+// // { id: 4, name: 'Superman', squad: 'Justice League' }
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  },
+  findOne: function(query) {
+    let valid = [];
+    return this.store.heroes.find(hero => {
+      Object.keys(query).forEach(key => {
+        valid.push(query[key] === hero[key]);
+      });
+      let result = valid.filter(Boolean).length === Object.keys(query).length;
+      valid = [];
+      return result;
+    });
+  }
+};
+
+console.log(Database.findOne({ id: 2 }));
+// { id: 2, name: 'Iron Man', squad: 'Avengers' }
