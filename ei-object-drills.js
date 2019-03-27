dffd065a7676dac7dc94d5f1634eee42c95bb961
MikeJung90 <kpnba90@gmail.com>
2019-03-27 17:05:47 -0400

    started bonus heros exercise

--- ei-object-drills.js
+++ ei-object-drills.js
@@ -146,5 +146,29 @@ const powerfulAttackers = characters.filter(character => character.attack > 5);
 
 
 
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query) {
+  
+}
 
 
+
+console.log(findOne(HEROES, { id: 1 }));
+// { id: 1, name: 'Captain America', squad: 'Avengers' }
+console.log(findOne(HEROES, { id: 10 }));
+// null
+console.log(findOne(HEROES, { id: 2, name: 'Aquaman' }));
+// null
+console.log(findOne(HEROES, { id: 5, squad: 'Justice League' }));
+// { id: 5, name: 'Wonder Woman', squad: 'Justice League' }
+console.log(findOne(HEROES, { squad: 'Justice League' }));
+// { id: 4, name: 'Superman', squad: 'Justice League' }
