18bb12b515a65d9c01fa43dcc498982474f9d97b
MikeJung90 <kpnba90@gmail.com>
2019-03-27 12:44:54 -0400

    validate

--- app.js
+++ app.js
@@ -98,3 +98,33 @@ function findById(data, id) {
 }
 
 // console.log(findById(data, 2));
+
+
+const objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago',
+};
+
+const objectB = {
+  id: 3,
+  age: 33,
+  city: 'Peoria',
+};
+
+const expectedKeys = ['id', 'name', 'age', 'city'];
+
+function validateKeys(object, expectedKeys) {
+  if (Object.keys(object).length !== expectedKeys.length) {
+    return false;
+  }
+
+  for (let i = 0; i < expectedKeys.length; i++) {
+    if (expectedKeys[i] in object === false) {
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(validateKeys(objectA, expectedKeys));
