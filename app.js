07defa58116979d77f486eebf1c01577df436a7a
Michael Verdi <michael.v.verdi@gmail.com>
2019-03-27 11:26:17 -0400

    createMyObject added

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+      return 'Hello'; 
+    }
+  };
+}
+
+// console.log(createMyObject());
+// console.log(createMyObject().sayHello());
+
+
